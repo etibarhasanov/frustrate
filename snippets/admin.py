563e1da016b75c7a4390d318e4863e8e7c6a8169
etibar hasanov <hasanovetibar@gmail.com>
2017-10-03 16:29:58 +0300

    add statistic tabel to admin page in #django

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from snippets.models import Statistics
 
 from django.contrib import admin
+admin.site.register(Statistics)
+
 
 # Register your models here.
