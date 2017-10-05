d0de28736ac425927b8f8918f88478025313c587
etibar hasanov <hasanovetibar@gmail.com>
2017-10-05 15:30:33 +0300

    training formula

--- snippets/views.py
+++ snippets/views.py
@@ -7,6 +7,10 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import pandas as pd
+import pickle
 
 
 class SnippetList(APIView):
@@ -15,12 +19,19 @@ class SnippetList(APIView):
     """
 
     def get(self, request, format=None):
+        # import numpy as np
+        # import pandas as pd
+        # from sklearn.ensemble import RandomForestClassifier
+        # from sklearn.model_selection import train_test_split
         # df = pd.DataFrame(list(Statistics.objects.all().values()))
-        # df_x=df.iloc[:,[0,1,2,3,4,6,7,8]]
+        # df_x=df.iloc[:,[0,1,2,3,5,7,8]]
         # df_y=df.iloc[:,5]
         # x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2, random_state=4)
         # rf=RandomForestClassifier(n_estimators=100)
         # rf.fit(x_train,y_train)
+        # pickle_out=open("data.pkl","wb")
+        # pickle.dump(rf, pickle_out)
+        # pickle_out.close()
 
         snippets = Statistics.objects.all()
         serializer = StatisticsSerializer(snippets, many=True)
@@ -29,7 +40,16 @@ class SnippetList(APIView):
     def post(self, request, format=None):
         serializer = StatisticsSerializer(data=request.data)
         if serializer.is_valid():
+            pickle_in = open('data.pkl', 'rb')
+            rf = pickle.load(pickle_in)
             serializer.save()
+            print rf.predict([[serializer.data['agentId'],
+                               serializer.data['averageDuration'],
+                               serializer.data['averageHoldingDuration'],
+                               serializer.data['externalId'],
+                               serializer.data['isResolved'],
+                               serializer.data['numberOfCalls'],
+                               serializer.data['numberOfEmails']]])
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -63,6 +83,7 @@ class SnippetDetail(APIView):
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 class Analyze(APIView):
     """
     List all snippets, or create a new snippet.
@@ -72,4 +93,3 @@ class Analyze(APIView):
         snippets = Statistics.objects.all()
         serializer = StatisticsSerializer(snippets, many=True)
         return Response(serializer.data)
-
